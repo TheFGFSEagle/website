a0fa1b6bd0c779fa0d9319f70bf47a14e61128fc
TheEagle <thefgfseagle@gmail.com>
2022-09-07 19:39:01 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,5 +2,5 @@
 import http.server
 import os
 
-s = http.HTTPServer(("0.0.0.0", os.environ.get("PORT")), http.server.CGIHTTPRequestHandler)
+s = http.server.HTTPServer(("0.0.0.0", os.environ.get("PORT")), http.server.CGIHTTPRequestHandler)
 s.server_forever()
