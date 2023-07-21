5dd198935fa0baf5cfa867b01ec11d14f4a10bbc
TheFGFSEagle <thefgfseagle@gmail.com>
2023-07-21 22:32:36 +0200

    Basic layout

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta encoding="utf-8"/>
-        <title>TheEagle's place</title>
-        <link rel="stylesheet" href="/style/main.css"/>
-    </head>
-    <body>
-        <header>
-            <h1>TheEagle's place</h1>
-            <p>FlightGear stuff, programming, ...</p>
-        </header>
-        <main>
-            <p>Hello ! I'm TheEagle, and this is my website where you can find various things, for example FlightGear videos and screenshots, or programming tutorials.</p>
-            <?php
-                echo "<h2>Hello from PHP</h2>";
-            ?>
-        </main>
-    </body>
+	<head>
+		<meta encoding="utf-8"/>
+		<title>TheEagle's place</title>
+		<link rel="stylesheet" href="/style/main.css"/>
+		<link rel="stylesheet" href="/style/menu.css"/>
+	</head>
+	<body>
+		<?php
+			include("static/header.html");
+		?>
+		<?php
+			include("static/menu.html");
+		?>
+		<?php
+			include("static/sidebar.html");
+		?>
+		
+		<main>
+			<p>
+				Hi ! I'm TheEagle, and this is my website where you can find all my activity, like my blog,
+				programming tutorials, my YouTube videos or nature photos. Welcome ! 😃️
+			</p>
+		</main>
+		
+		<?php
+			include("static/footer.html");
+		?>
+	</body>
 </html>
